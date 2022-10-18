2cf70f407163e99b92c5060c33dc095fe86f30ad
Sharath <sharathpavan@rehlat.com>
2022-10-18 21:00:43 +0530

    Added Image view to the row. Added paddings and spaces in the layout.

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -3,15 +3,21 @@ package com.example.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,9 +44,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MessageCard(msg: MainActivity.Message) {
-    Column() {
-        Text(text = msg.author)
-        Text(text = msg.message)
+    Row(
+        modifier = Modifier.padding(all = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture ),
+            contentDescription = "profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(shape = CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+        
+        Column() {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.height(5.dp))
+            Text(text = msg.message)
+        }
     }
 }
 
